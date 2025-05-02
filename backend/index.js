3c20bc28bb9af46b90d0914e7d6acb4d83b347f0
Richard Hill <richard.hill@bentley.com>
2025-05-02 03:00:07 +0100

    Adding the new summary route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require('dotenv').config();
 
 const app = express();
 const PORT = 3001;
@@ -14,6 +15,10 @@ app.use("/scrape", scrapeRoutes);
 
 const spreadsheetRoutes = require("./routes/spreadsheet");
 app.use("/excel", spreadsheetRoutes);
+
+const summaryRoute = require('./routes/summary');
+app.use('/summary', summaryRoute);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
